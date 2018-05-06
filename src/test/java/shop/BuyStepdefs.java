53d07f1d4eedb872d4a81bc9add92be69ca22054
Wee Sritippho <weeaix@gmail.com>
2018-05-06 20:01:43 +0700

    Extracted initialization to a separated setup method

--- src/test/java/shop/BuyStepdefs.java
+++ src/test/java/shop/BuyStepdefs.java
@@ -1,6 +1,7 @@
 package shop;
 
 import cucumber.api.DataTable;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
@@ -13,16 +14,20 @@ public class BuyStepdefs {
     private ProductCatalog catalog;
     private Order order;
 
+    @Before
+    public void setup() {
+        catalog = new ProductCatalog();
+        order = new Order();
+    }
+
     @Given("^a product \"([^\"]*)\" with price (.+) exists$")
     public void a_product_with_price_exists(String name, double price) {
-        catalog = new ProductCatalog();
         catalog.addProduct(name, price);
     }
 
     @Given("^the following products exist:$")
     public void the_following_products_exist(DataTable table) {
         Map<String,Double> data = table.asMap(String.class, Double.class);
-        catalog = new ProductCatalog();
 
         for (String name : data.keySet()) {
             double price = data.get(name);
@@ -33,7 +38,6 @@ public class BuyStepdefs {
     @When("^I buy \"([^\"]*)\" with quantity (\\d+)$")
     public void i_buy_with_quantity(String name, int quant) {
         Product prod = catalog.getProduct(name);
-        order = new Order();
         order.addItem(prod, quant);
     }
 
